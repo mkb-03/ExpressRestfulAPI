a9b570b82597abaf10afb9ef24b707048f58254f
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-05 23:26:39 +0500

    Learning ExpressRestfulAPI

--- index.js
+++ index.js
@@ -7,5 +7,12 @@ app.get('/' , (req, res)=>{
     res.send("Hello Express")
 })
 
+app.get('/blog' , (req, res)=>{
+
+    res.send("Hello Blog route")
+})
+
+
+
 
 app.listen(3000 , ()=> console.log("Listening on port 3000..."))
