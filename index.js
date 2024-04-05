5af95c6fab1b07aac58924faff348b1e86f9a3b6
Muhammad Khizran <muhammadkhizranbutt@gmail.com>
2024-04-05 23:46:33 +0500

    Learning ExpressRestfulAPI

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express();
 
-app.get('/' , (req, res)=>{
+app.get('/', (req, res) => {
 
     res.send("Hello Express")
 })
 
-app.get('/blog' , (req, res)=>{
+app.get('/blog', (req, res) => {
 
     res.send("Hello Blog route")
 })
@@ -15,4 +15,11 @@ app.get('/blog' , (req, res)=>{
 
 
 
-app.listen(3000 , ()=> console.log("Listening on port 3000..."))
+mongoose.connect('mongodb://127.0.0.1:27017/expressAPI')
+    .then(() => {
+        console.log('Connected to MongoDB')
+        app.listen(3000, () => console.log("Listening on port 3000..."))
+    })
+    .catch((error => {
+        console.log(error)
+    }))
